df9c1d7b6bca929d48d55291dd9c5175688de727
ZyxMapLe <87278112+ZyxMapLe@users.noreply.github.com>
2021-10-19 15:57:22 +0700

    Update values.js

--- src/config/values.js
+++ src/config/values.js
@@ -14,6 +14,6 @@ exports.subject_email = "Pemalas APi | Verification";
  * Configurasi database
  */
 exports.mongoUrl =
-  "mongodb+srv://*****:******@cluster0.mn1td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://Zyxmaple:ramajelek@cluster0.mn1td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.dbGet = "pemalasapi";
 exports.msgSuccesConnect = "Connected correctly to server";
