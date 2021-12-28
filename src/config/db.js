859e4acdcfe185306b5873ba7d3d1ffffbaf1c3d
Rizki Ramadan <87278112+ZyxMapLe@users.noreply.github.com>
2021-12-28 20:27:12 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
 const config = require("../config/values");
 const monk = require("monk");
+const mongoose = require('mongoose');
 const url = config.mongoUrl;
-const db = monk(url);
-
-db.then(() => {
-  console.log(config.msgSuccesConnect);
-});
-
-const PemalasDB = db.get(config.dbGet);
+function PemalasDB() {
+    mongoose.connect(dbURI, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+      console.log('[INFO] Connect to DB success!');
+    });
+};
 
 module.exports = PemalasDB;
