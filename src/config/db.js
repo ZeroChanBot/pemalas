d30d3daff2628029c7c26a7ac2f364e2d56e9caa
Rizki Ramadan <87278112+ZyxMapLe@users.noreply.github.com>
2021-12-28 20:47:13 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const config = require("../config/values");
 const monk = require("monk");
 const mongoose = require('mongoose');
-const url = config.mongoUrl;
+const url = 'mongodb+srv://Zyxmaple:ramajelek@cluster0.mn1td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 function PemalasDB() {
     mongoose.connect(url, { 
       useNewUrlParser: true, 
